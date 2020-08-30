843d74e4b6133c7c1a1c0845bca9bfe2421a52d4
Ayan Mullick <ayanmullick@gmail.com>
2020-08-30 00:01:29 -0500

    Update RGcreation.ps1

--- AzPS/RGcreation.ps1
+++ AzPS/RGcreation.ps1
@@ -1,10 +1,12 @@
-$Secret = ConvertTo-SecureString -String 'Passw0rd' -AsPlainText -Force -Verbose
-$pscredential = New-Object -TypeName System.Management.Automation.PSCredential('29396ed6-d65a-4886-80e9-eb4889bb25d3',$Secret) -Verbose
+#$Secret = ConvertTo-SecureString -String '' -AsPlainText -Force -Verbose
+#$pscredential = New-Object -TypeName System.Management.Automation.PSCredential('29396ed6-d65a-4886-80e9-eb4889bb25d3',$Secret) -Verbose
 
+$pscredential = New-Object -TypeName System.Management.Automation.PSCredential('29396ed6-d65a-4886-80e9-eb4889bb25d3',$secrets.SECRET) -Verbose
 $secrets.SECRET
 
 Install-Module -Name Az.Accounts,Az.Resources -Force -SkipPublisherCheck 
 
 Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant 1054d918-a880-46d0-aa19-2dd47c7588d3 -Subscription 76299958-463a-4b22-80cc-45af1e994418 -Verbose
 
-New-AzResourceGroup -Location SouthCentralUS -Name GitHubActions -Verbose
+#New-AzResourceGroup -Location SouthCentralUS -Name GitHubActions -Verbose
+Get-AzResourceGroup

67cdff7ec0421a11c82d5fb4df4e2a370a95f748
Ayan Mullick <ayanmullick@gmail.com>
2020-08-29 23:18:23 -0500

    Update RGcreation.ps1

--- AzPS/RGcreation.ps1
+++ AzPS/RGcreation.ps1
@@ -1,8 +1,7 @@
-$Secret = ConvertTo-SecureString -String 'Passw0rd' -AsPlainText -Force
-$pscredential = New-Object -TypeName System.Management.Automation.PSCredential('29396ed6-d65a-4886-80e9-eb4889bb25d3',$Secret)
+$Secret = ConvertTo-SecureString -String 'Passw0rd' -AsPlainText -Force -Verbose
+$pscredential = New-Object -TypeName System.Management.Automation.PSCredential('29396ed6-d65a-4886-80e9-eb4889bb25d3',$Secret) -Verbose
 
-Install-Module -Name Az.Accounts,Az.Resources -Force -SkipPublisherCheck -AllowClobber -Verbose
-#Import-Module Az.Accounts,Az.Resources -Verbose
+Install-Module -Name Az.Accounts,Az.Resources -Force -SkipPublisherCheck 
 
 Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant 1054d918-a880-46d0-aa19-2dd47c7588d3 -Subscription 76299958-463a-4b22-80cc-45af1e994418 -Verbose
 

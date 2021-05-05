cb8d9b656fada6ac291bd506ad122dd69f1f4796
Ayan Mullick <ayanmullick@gmail.com>
2021-05-04 20:43:18 -0500

    Update RGcreation.ps1

--- AzPS/RGcreation.ps1
+++ AzPS/RGcreation.ps1
@@ -1,4 +1,4 @@
-#$Secret = ConvertTo-SecureString -String '' -AsPlainText -Force -Verbose
+$Secret = ConvertTo-SecureString -String 'AgSg5r~E4k4hd.4mauwOhV-pC8wQh.~66V' -AsPlainText -Force -Verbose
 #$pscredential = New-Object -TypeName System.Management.Automation.PSCredential('29396ed6-d65a-4886-80e9-eb4889bb25d3',$Secret) -Verbose
 
 #$pscredential = New-Object -TypeName System.Management.Automation.PSCredential('29396ed6-d65a-4886-80e9-eb4889bb25d3',$secrets.SECRET) -Verbose
@@ -12,6 +12,6 @@
 #Get-AzResourceGroup
 
 #V2
-$pscredential = New-Object -TypeName System.Management.Automation.PSCredential('29396ed6-d65a-4886-80e9-eb4889bb25d3',$secrets.SECRET)
+$pscredential = New-Object -TypeName System.Management.Automation.PSCredential('29396ed6-d65a-4886-80e9-eb4889bb25d3',$Secret)
 
 Get-AzResourceGroup -DefaultProfile (Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant 1054d918-a880-46d0-aa19-2dd47c7588d3 -Subscription 76299958-463a-4b22-80cc-45af1e994418)|ft

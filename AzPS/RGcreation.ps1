e0214f139348ca94d4543463e99fe4e2c0d2dd0d
Ayan Mullick <ayanmullick@gmail.com>
2021-05-04 20:39:10 -0500

    Update RGcreation.ps1

--- AzPS/RGcreation.ps1
+++ AzPS/RGcreation.ps1
@@ -1,12 +1,17 @@
 #$Secret = ConvertTo-SecureString -String '' -AsPlainText -Force -Verbose
 #$pscredential = New-Object -TypeName System.Management.Automation.PSCredential('29396ed6-d65a-4886-80e9-eb4889bb25d3',$Secret) -Verbose
 
-$pscredential = New-Object -TypeName System.Management.Automation.PSCredential('29396ed6-d65a-4886-80e9-eb4889bb25d3',$secrets.SECRET) -Verbose
-$secrets.SECRET
+#$pscredential = New-Object -TypeName System.Management.Automation.PSCredential('29396ed6-d65a-4886-80e9-eb4889bb25d3',$secrets.SECRET) -Verbose
+#$secrets.SECRET
 
-Install-Module -Name Az.Accounts,Az.Resources -Force -SkipPublisherCheck 
+#Install-Module -Name Az.Accounts,Az.Resources -Force -SkipPublisherCheck 
 
-Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant 1054d918-a880-46d0-aa19-2dd47c7588d3 -Subscription 76299958-463a-4b22-80cc-45af1e994418 -Verbose
+#Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant 1054d918-a880-46d0-aa19-2dd47c7588d3 -Subscription 76299958-463a-4b22-80cc-45af1e994418 -Verbose
 
 #New-AzResourceGroup -Location SouthCentralUS -Name GitHubActions -Verbose
-Get-AzResourceGroup
+#Get-AzResourceGroup
+
+#V2
+$pscredential = New-Object -TypeName System.Management.Automation.PSCredential('29396ed6-d65a-4886-80e9-eb4889bb25d3',$secrets.SECRET)
+
+Get-AzResourceGroup -DefaultProfile (Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant 1054d918-a880-46d0-aa19-2dd47c7588d3 -Subscription 76299958-463a-4b22-80cc-45af1e994418)|ft

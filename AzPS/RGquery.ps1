88621216baad716fe899719dbbc1f231b7027fa5
Ayan Mullick <ayanmullick@gmail.com>
2023-05-27 00:41:55 -0500

    Update RGquery.ps1

--- AzPS/RGquery.ps1
+++ AzPS/RGquery.ps1
@@ -1,7 +1,10 @@
-Install-Module -Name Az.Accounts,Az.Resources -Force -SkipPublisherCheck 
+<#Install-Module -Name Az.Accounts,Az.Resources -Force -SkipPublisherCheck 
 
 $Secret = ConvertTo-SecureString -String "$`{{secrets.PSGITHUBACTIONSV2_SPN_PSWD}}" -AsPlainText -Force -Verbose
 $pscredential = New-Object -TypeName System.Management.Automation.PSCredential('865d9db9-7a02-4bf2-9b7b-33d108f42da5',$Secret)
 
 Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant 1054d918-a880-46d0-aa19-2dd47c7588d3 -Subscription 66f0b393-314b-4a8f-8aec-df3a9872eb3e -Verbose
 Get-AzResourceGroup |ft
+#>
+
+Write-host "$`{{secrets.PSGITHUBACTIONSV2_SPN_PSWD}}"

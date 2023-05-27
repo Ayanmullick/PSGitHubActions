a9a917bd0a0d6bfa36e607e5760261eb806f8fee
Ayan Mullick <ayanmullick@gmail.com>
2023-05-27 01:17:33 -0500

    Update RGquery.ps1

--- AzPS/RGquery.ps1
+++ AzPS/RGquery.ps1
@@ -7,4 +7,4 @@ Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant 1054d918-a
 Get-AzResourceGroup |ft
 #>
 
-Write-host $`{{secrets.PSGITHUBACTIONSV2_SPN_PSWD}}
+Write-host ${{secrets.PSGITHUBACTIONSV2_SPN_PSWD}}
